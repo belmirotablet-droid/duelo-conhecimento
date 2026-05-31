81c7e51568507b052baa52f635443a31baa9feb4
belmirotablet-droid <belmirotablet@gmail.com>
2026-05-30 21:56:26 -0300

    Create personagens.js

---
+++ personagens.js
@@ -0,0 +1,74 @@
+const PERSONAGENS = [
+  {
+    id: "verbantis",
+    nome: "Verbantis",
+    disciplina: "Língua Portuguesa",
+    imagem: "img/verbantis.png"
+  },
+  {
+    id: "globite",
+    nome: "Globite",
+    disciplina: "Inglês",
+    imagem: "img/globite.png"
+  },
+  {
+    id: "colorius",
+    nome: "Colorius",
+    disciplina: "Arte",
+    imagem: "img/colorius.png"
+  },
+  {
+    id: "energix",
+    nome: "Energix",
+    disciplina: "Educação Física",
+    imagem: "img/energix.png"
+  },
+  {
+    id: "calcubot",
+    nome: "Calcubot",
+    disciplina: "Matemática",
+    imagem: "img/calcubot.png"
+  },
+  {
+    id: "biotryx",
+    nome: "Biotryx",
+    disciplina: "Biologia",
+    imagem: "img/biotryx.png"
+  },
+  {
+    id: "fotonix",
+    nome: "Fotonix",
+    disciplina: "Física",
+    imagem: "img/fotonix.png"
+  },
+  {
+    id: "chemion",
+    nome: "Chemion",
+    disciplina: "Química",
+    imagem: "img/chemion.png"
+  },
+  {
+    id: "tempus",
+    nome: "Tempus",
+    disciplina: "História",
+    imagem: "img/tempus.png"
+  },
+  {
+    id: "terralux",
+    nome: "Terralux",
+    disciplina: "Geografia",
+    imagem: "img/terralux.png"
+  },
+  {
+    id: "socion",
+    nome: "Socion",
+    disciplina: "Sociologia",
+    imagem: "img/socion.png"
+  },
+  {
+    id: "sapientia",
+    nome: "Sapientia",
+    disciplina: "Filosofia",
+    imagem: "img/sapientia.png"
+  }
+];
